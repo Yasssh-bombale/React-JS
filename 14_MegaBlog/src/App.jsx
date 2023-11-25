cb2a554a6af7f8776841e8378f1e581079b6e3c5
Yash Vijay Bombale <yashbombale@gmail.com>
2023-11-25 21:45:46 +0530

    intialize an reduxToolkit

--- 14_MegaBlog/src/App.jsx
+++ 14_MegaBlog/src/App.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 function App() {
-  return <h1>hey there</h1>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => console.log(`error :: getCurrentUser()::${error}`))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen border flex flex-wrap content-between ">
+      <div className="w-full ">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
